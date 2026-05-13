bd415963960f5d6c79299eb5e3a231f715e15ab5
otaviobrignoni <otavio.678.b@gmail.com>
2026-05-13 15:07:15 -0300

    Ajustado projeto para SDK Web e eemovido módulos de UI

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -1,13 +1,8 @@
-﻿using System.Text;
-using GestaoDeEquipamentos.ConsoleApp.Shared;
-
-namespace GestaoDeEquipamentos.ConsoleApp;
+﻿namespace GestaoDeEquipamentos.ConsoleApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.OutputEncoding = Encoding.UTF8;
-        UserInterface.MainMenu();
     }
 }
