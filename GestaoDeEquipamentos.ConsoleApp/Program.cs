9d3644713cfa2fd19df8638f5d94614c8e227575
otaviobrignoni <otavio.678.b@gmail.com>
2026-05-17 12:14:15 -0300

    Implementada inicialização do webapp na program

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -5,6 +5,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
         
+        builder.Services.AddControllersWithViews();
+
+        WebApplication app = builder.Build();
+
+        app.UseRouting();
+        app.MapDefaultControllerRoute();
+
+        app.Run();
     }
 }
